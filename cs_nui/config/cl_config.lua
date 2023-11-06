5a2e75c134b2c4dc411db536c3dd149bf372bd7e
Chabo-Service Pasha <149976015+Chabo-Service-Pasha@users.noreply.github.com>
2023-11-07 00:34:57 +0100

    Add files via upload

---
+++ cs_nui/config/cl_config.lua
@@ -0,0 +1,7 @@
+FS = {}
+
+FS.Type = "ingame" -- possible: "ingame" / "saltychat"
+
+FS.Title = "VOICE IS NOT CONNECTED"
+FS.Description = "Please check your GTAV Settings"
+FS.DiscordLink = "DISCORD.GG/CHABO-SERVICE"
---
+++ cs_nui/fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+version "1.0"
+author "CS-CC.de"
+description "CS_NUI - Made by Chabo-Service"
+
+client_scripts {
+    "config/cl_config.lua",
+    "client/*.lua"
+}
+
+server_scripts {
+    "server/*.lua"
+}
+
+ui_page "html/index.html"
+
+files {
+    "html/*.css",
+    "html/*.js",
+    "html/*.png",
+    "html/*.svg",
+    "html/*.html"
+}
+
+escrow_ignore {
+    "config/*.lua"
+}
+dependency '/assetpacks'
